28ec43456a0095e1928dff9f1149397f332e2228
JitSingha46 <140158691+JitSingha46@users.noreply.github.com>
2023-07-26 01:47:21 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/JitSingha46/url-auto-delete-shortener-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /url-auto-delete-shortener-bot
 fi
-cd /Eva
+cd /url-auto-delete-shortener-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
